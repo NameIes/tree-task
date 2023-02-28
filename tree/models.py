8ba0c8034883a7da703af64b6c67fe64ab63dca8
Nameies <nameies@yandex.ru>
2023-02-28 15:28:45 +0300

    Models created and added to admin panel.

--- tree/models.py
+++ tree/models.py
@@ -1,3 +1,27 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class TreeMenu(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class TreeItem(models.Model):
+    name = models.CharField(max_length=80)
+    menu = models.ForeignKey(TreeMenu, on_delete=models.CASCADE, related_name='tree_items', blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='childrens', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        menu = bool(self.menu)
+        parent = bool(self.parent)
+
+        if not ((not menu and parent) or (menu and not parent)):
+            raise ValidationError({
+                'menu': 'Only one of "menu" and "parent" must be filled.'
+            })
